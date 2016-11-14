86265ea3d538bb0a0fbe395893586eab9a1bf818
chenfei1 <chenfei1@7F-Chenfei1.globale.com>
2016-11-14 18:04:13 +0800

    添加链表相关测试代码

---
+++ dubbo-consumer-webapp/src/main/java/com/allen/test/LinkedAndQueue.java
@@ -0,0 +1,107 @@
+package com.allen.test;
+
+/**
+ * 模拟单向链表
+ * All rights Reserved, Designed By HQYG
+ * Copyright:   Copyright(C) 2016
+ * Company:     HQYG.
+ * author:      cf
+ * Createdate:  2016年11月14日下午5:12:20
+ */
+public class LinkedAndQueue {
+	
+	//头节点
+	Node head;
+	
+	/**
+	 * 定义一个链表节点对象
+	 * All rights Reserved, Designed By HQYG
+	 * Copyright:   Copyright(C) 2016
+	 * Company:     HQYG.
+	 * author:      cf
+	 * Createdate:  2016年11月14日下午5:12:41
+	 */
+	class Node{
+		
+		Node nextNode;
+		String content;
+		
+		public Node getNextNode() {
+			return nextNode;
+		}
+		
+		public void setNextNode(Node nextNode) {
+			this.nextNode = nextNode;
+		}
+		
+		public String getContent() {
+			return content;
+		}
+		
+		public void setContent(String content) {
+			this.content = content;
+		}
+
+		public Node(String content) {
+			super();
+			this.content = content;
+		}
+
+		@Override
+		public String toString() {
+			return "Node [nextNode=" + nextNode + ", content=" + content + "]";
+		}
+	}
+
+	
+	/**
+	 * 向链表中添加节点
+	 * @param content
+	 */
+	public void addNode(String content){
+		Node node = new Node(content);
+		if(head == null){
+			head = node;
+			return;
+		}
+		
+		Node temp = head;
+		if(temp.nextNode !=null){
+			temp = temp.nextNode;
+		}
+		temp.nextNode = node;
+	}
+	
+	
+	
+	//打印出链表信息
+	public void printList(){
+		Node headNode = head;
+		while(headNode!=null){
+			System.out.println("node content:" + headNode.getContent() );
+			if(headNode.getNextNode() != null)
+			{
+				System.out.println("nextNode content:" + headNode.getNextNode().getContent());
+			}
+			headNode = headNode.nextNode;
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		LinkedAndQueue link = new LinkedAndQueue();
+		link.addNode("1");
+		link.addNode("2");
+		link.addNode("3");
+		link.addNode("4");
+		link.addNode("5");
+		link.printList();
+		
+		System.out.println("link head data:" + link.head.getContent());
+		if(link.head.getNextNode() != null){
+			System.out.println("link head nextNode content:" + link.head.getNextNode().getContent());
+		}
+
+	}
+
+}
--- dubbo-consumer-webapp/src/main/java/com/allen/test/Test.java
+++ dubbo-consumer-webapp/src/main/java/com/allen/test/Test.java
@@ -1,11 +1,46 @@
 package com.allen.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test {
 	
+	private String name;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
 	public static void main(String[] args) {
 		int i = 0x00000001;
 		System.out.println(0 & i);
 		//boolean flag = 1 & i;
+		
+		String s ="aa";
+		int code = s.hashCode();
+		System.out.println(code);
+		Map<String, Object> map = new HashMap<>();
+		String value1 = "value1";
+		String v = (String) map.put("key1", value1);
+		System.out.println(v);
+		
+		Test test = new Test();
+		test.setName("allen");
+		
+		System.out.println(test.getName());
+		
+		Test test2 = test;
+		test2.setName("viney");
+		
+		System.out.println(test.getName());
 		
 	}
 
