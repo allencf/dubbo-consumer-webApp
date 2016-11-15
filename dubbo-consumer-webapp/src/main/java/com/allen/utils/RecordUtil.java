56b8f40d3192b96ec50efaf00bad210b710fb541
chenfei1 <chenfei1@7F-Chenfei1.globale.com>
2016-11-15 17:09:16 +0800

    添加record的工具类

---
+++ dubbo-consumer-webapp/src/main/java/com/allen/utils/RecordUtil.java
@@ -0,0 +1,22 @@
+package com.allen.utils;
+
+public class RecordUtil {
+
+	/**
+	 * 记录的一个方法
+	 */
+	public void recordMethod(){
+		
+		//shrio
+		//http://blog.csdn.net/chris_mao/article/details/49288251
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
