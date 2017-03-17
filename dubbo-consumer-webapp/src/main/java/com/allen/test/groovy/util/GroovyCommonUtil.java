16d8fe765c9c2cb03816b2d0610e4b413fd2bf26
allen <allen@7F-allen.globale.com>
2017-03-17 17:23:04 +0800

    提交测试代码………………

--- dubbo-consumer-webapp/src/main/java/com/allen/test/groovy/util/GroovyCommonUtil.java
+++ dubbo-consumer-webapp/src/main/java/com/allen/test/groovy/util/GroovyCommonUtil.java
@@ -1,5 +1,13 @@
 package com.allen.test.groovy.util;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import groovy.lang.GroovyObject;
+import groovy.util.GroovyScriptEngine;
+import groovy.util.ResourceException;
 
 /**
  * GroovyCommonUtil操作的一个工具类
@@ -11,6 +19,53 @@ package com.allen.test.groovy.util;
  */
 public class GroovyCommonUtil {
 
+	private static final Logger log = LoggerFactory.getLogger(GroovyCommonUtil.class);
+    //该变量用于指明groovy脚本所在的父目录
+    static String root[]=new String[]{"bin/groovy/"};  
+    static GroovyScriptEngine groovyScriptEngine;  
+    
+    static{
+        try {
+            groovyScriptEngine=new GroovyScriptEngine(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }  
+    }
+    
+    /**
+     *  用于调用指定Groovy脚本中的指定方法 
+     * @param scriptName    脚本名称
+     * @param methodName    方法名称
+     * @param params        方法参数
+     * @return
+     */
+	@SuppressWarnings({ "rawtypes"})
+	public Object invokeMethod(String scriptName, String methodName, Object... params) throws Exception{
+	    Object ret = null;
+	    Class scriptClass = null;
+	    GroovyObject scriptInstance = null;
+	    
+	    try {
+	        scriptClass = groovyScriptEngine.loadScriptByName(scriptName);
+	        scriptInstance = (GroovyObject)scriptClass.newInstance();
+	    } catch (Exception e1) {
+	        log.warn("加载脚本["+scriptName+"]出现异常", e1);
+	        throw new Exception("加载脚本"+scriptName+"失败");
+	    }
+	
+	    try {
+	        ret = (String)scriptInstance.invokeMethod(methodName, params);
+	    } catch (IllegalArgumentException e) {
+	        log.warn("执行方法" + methodName + "参数出现异常, 参数为" + params, e);
+	        throw new Exception("调用方法[" + methodName + "]失败，因参数不合法");
+	    } catch(Exception e){
+	        log.warn("执行方法" + methodName + "出现异常", e);
+	        throw new Exception("调用方法[" + methodName + "]失败");
+	    }
+	
+	    return ret;
+	}
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
