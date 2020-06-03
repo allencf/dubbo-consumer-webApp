5c0ddb62c33dc12f6f83687204916145c5067cb1
chenfei1 <chenfei1@globalegrow.com>
2020-06-03 10:10:46 +0800

    commit

--- dubbo-consumer-webapp/src/main/java/com/allen/test/redis/RedisClient.java
+++ dubbo-consumer-webapp/src/main/java/com/allen/test/redis/RedisClient.java
@@ -4,9 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
